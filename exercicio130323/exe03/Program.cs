a3f2eb11a79243cc6b8b3c15e4eab44cc9efa7dc
VitorDD <vitordevila@gmail.com>
2023-03-13 21:51:27 -0300

    Prog3_23

---
+++ exercicio130323/exe03/Program.cs
@@ -0,0 +1,19 @@
+﻿static void FormatString(
+    string stringToFormat
+) {
+stringToFormat.Replace("Mundo", "Marte");
+}
+
+static string FormatReturningString(string stringToFormat)
+    {
+        return stringToFormat.Replace("Mundo", "Marte");
+    }
+
+    var mensagem = "olá, Mundo!!";
+    // invocando o metodo sem retorno
+    FormatString(mensagem);
+Console.WriteLine(mensagem);
+
+var outramensagem = "bom dia Mundo!";
+Console.WriteLine( FormatReturningString(outramensagem) );
+
---
+++ exercicio130323/exe04/Program.cs
@@ -0,0 +1,31 @@
+﻿var myFloat = 10f;
+float meuFloat =11;
+
+//conversão de tipo numerico
+
+int myInteger = 100;
+meuFloat = (float)myInteger;
+
+decimal myDecimal = Convert.ToDecimal(myInteger);
+
+var myDouble = 10d;
+myDecimal = 10m;
+
+string meuNumeroTexto = Convert.ToString(myInteger);
+int qtdChars = meuNumeroTexto.Length;
+for(int i = 0; i < qtdChars; i++)
+//laço de repetição
+//qtdChars = flag
+//i++ acumulador
+//i=0 contador
+
+{
+Console.WriteLine( meuNumeroTexto[i] );
+}
+
+
+
+
+
+
+

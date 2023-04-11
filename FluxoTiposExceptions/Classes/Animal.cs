b30cc823b077ee77212620bf0cb72d81a7a101dc
VitorDD <vitordevila@gmail.com>
2023-04-10 22:07:34 -0300

    aula 10/04/23

---
+++ FluxoTiposExceptions/Classes/Animal.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluxoTiposExceptions.Classes
+{
+    // animal é um super tipo
+    //uma classe que contém atributos e 
+    // especificações que são genericas
+    
+    public class Animal
+    {
+        public string Name;
+        public DateTime Born;
+        public byte legs;
+    }
+}

f59258476802369bcfbc65a283bc0bb826221e75
VitorDD <118028680+VitorDD@users.noreply.github.com>
2023-07-01 18:26:52 -0300

    Trabalho Final de PG1

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Arquivos.Views;
+
+
+int option = 0;
+
+do
+{
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("Programa para leitura e exportação de dados");
+    Console.WriteLine("*******************************************");
+    Console.WriteLine();
+    Console.WriteLine("1 - Clientes");
+    Console.WriteLine("2 - Animais");
+    Console.WriteLine("3 - Veterinários");
+    Console.WriteLine("4 - Clinica");
+
+    option = Convert.ToInt32(Console.ReadLine());
+
+    switch(option)
+    {
+        case 1 :
+            Console.WriteLine("Opção Clientes");
+            ClientView clientView = new ClientView();
+        break;
+        case 2 :
+            Console.WriteLine("Opção Animais");
+            AnimalView animalView = new AnimalView();
+        break;
+         case 3 :
+            Console.WriteLine("Opção Veterinarios");
+            VeteView veteView = new VeteView();
+        break;
+         case 4 :
+            Console.WriteLine("Opção Clinica");
+            ClinicView clinicView = new ClinicView();
+        break;
+    }
+
+}while( option > 0 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
